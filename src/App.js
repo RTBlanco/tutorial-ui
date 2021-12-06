0612e06bdca20bb9215e77832a1472157cae0884
RTBlanco <ronnytoribio1@hotmail.com>
2021-12-06 13:43:59 -0500

    added styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import UserForm from './components/UserForm'
 import './App.css';
+import Banner from './components/Banner';
 
 function App() {
   return (
-    <Container fluid='lg'>
+    <Container fluid='sm'>
+      <Banner />
       <UserForm />
     </Container>
   );
