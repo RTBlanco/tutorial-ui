1e9daedffa8161561f5b985e6bd80773b951cfae
RTBlanco <ronnytoribio1@hotmail.com>
2021-12-06 14:25:30 -0500

    added ability to turn on overlay

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import UserForm from './components/UserForm'
 import './App.css';
 import Banner from './components/Banner';
+import {useState} from 'react'
 
 function App() {
+  const [active, setActive] = useState(false)
+
+  const toggleOverlay = () => {
+    setActive(prev => !prev)
+  }
+
   return (
-    <Container fluid='sm'>
-      <Banner />
-      <UserForm />
-    </Container>
+    <>
+      {active ? <div id="overlay" onClick={toggleOverlay}></div> : ''}
+      <Container fluid='sm'>
+        <Banner />
+        <UserForm />
+        <Row fluid="sm" className="d-flex justify-content-center align-items-center">
+          <Button variant="success" size="lg" style={{marginTop: "50px", width: "25%"}} onClick={toggleOverlay} >Start Tutorial</Button>
+        </Row>
+      </Container>
+    </>
+    
+
+    
   );
 }
 
