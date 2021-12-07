529409ac93ab058fda9b623e678e6c7887f919e7
RTBlanco <ronnytoribio1@hotmail.com>
2021-12-07 16:34:32 -0500

    handled what will happen if use completes form

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -4,11 +4,17 @@ import { useState } from 'react';
 const UserForm = () => {
   const [checked, setChecked] = useState(false);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let user = e.target[0].value
+    window.alert(`You have been signed as ${user}`)
+  }
+
   return (
-    <Form id="user-form">
+    <Form id="user-form" onSubmit={handleSubmit} >
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control id="email" type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter email" />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
@@ -16,10 +22,10 @@ const UserForm = () => {
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control id="password" type={checked ? "text" : "password"} placeholder="Password" />
+        <Form.Control  type={checked ? "text" : "current-password"} placeholder="Password" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check id="checkbox" type="checkbox" label="Show password" onChange={() => setChecked(prev => !prev)}/>
+        <Form.Check type="checkbox" label="Show password" onChange={() => setChecked(prev => !prev)}/>
       </Form.Group>
       <Button id="submit" variant="primary" type="submit">
         Submit
