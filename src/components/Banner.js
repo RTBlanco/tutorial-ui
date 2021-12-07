c842dc36069406cef64cd00862e78acee40ae767
RTBlanco <ronnytoribio1@hotmail.com>
2021-12-07 16:49:41 -0500

    removed fluid from row

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,7 @@ import "./Banner.css"
 
 const Banner = () => {
   return (
-    <Row id="banner"  className="d-flex justify-content-center align-items-center" fluid>
+    <Row id="banner"  className="d-flex justify-content-center align-items-center" >
       <h1 className="text-center">Tutorial UI</h1>
     </Row>
   );
